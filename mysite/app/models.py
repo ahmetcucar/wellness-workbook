6424cf9a0b6972d7f3960bbd66fe81ea16a83a33
ahmetcucar <acucar@uw.edu>
2024-01-02 23:53:43 -0800

    add post data model

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=True)  # Optional title
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title or f"Journal on {self.date_posted.strftime('%m.%d.%Y')}"
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'title': 'Journal 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2021'
-    },
-    {
-        'title': 'Journal 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2021'
-    },
-    {
-        'content': 'Third post content',
-        'date_posted': 'August 29, 2021'
-    }
-]
+# posts = [
+#     {
+#         'title': 'Journal 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2021'
+#     },
+#     {
+#         'title': 'Journal 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2021'
+#     },
+#     {
+#         'content': 'Third post content',
+#         'date_posted': 'August 29, 2021'
+#     }
+# ]
 
 # Create your views here.
 def home(request):
@@ -26,4 +27,7 @@ def habits(request):
     return render(request, 'app/habits.html')
 
 def journals(request):
-    return render(request, 'app/journals.html', {'posts': posts})
+    context = {
+        'posts': Post.objects.filter(author=request.user).order_by('-date_posted')
+    }
+    return render(request, 'app/journals.html', context)
