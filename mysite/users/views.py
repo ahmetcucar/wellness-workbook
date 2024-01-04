0edf83c9b9e42a651eea1cd76ef50ac7ce005aa9
ahmetcucar <acucar@uw.edu>
2024-01-03 22:33:28 -0800

    login/logout functionality

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -9,8 +9,9 @@ def register(request):
         if form.is_valid():
             form.save()
         username = form.cleaned_data.get('username')
-        messages.success(request, f'Account created for {username}!')
-        return redirect('app-habits')
+        messages.success(request,
+                         f'Account created for {username}! You can now log in.')
+        return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
