c90be29003f1ea1af2fa73ee6bde03dc7501a459
ahmetcucar <acucar@uw.edu>
2024-01-18 01:35:25 -0800

    fixed bug @user registration

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -9,10 +9,12 @@ def register(request):
         form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        username = form.cleaned_data.get('username')
-        messages.success(request,
-                         f'Account created for {username}! You can now log in.')
-        return redirect('login')
+            username = form.cleaned_data.get('username')
+            messages.success(request,
+                            f'Account created for {username}! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, f'Error creating account!')
     else:
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
