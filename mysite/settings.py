d5bc760e455a3341ecca6b684a83e348f112f8ef
ahmetcucar <acucar@uw.edu>
2024-01-19 21:03:47 -0800

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,10 +83,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "dbpostgresql",
